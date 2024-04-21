7d5911b8507f48431a976e2640ded25d75b2a109
Vicente Barrios <you@example.com>
2024-04-20 20:43:40 -0400

    new

--- Tarea1/problema3_antiguo.jl
+++ Tarea1/problema3_antiguo.jl
@@ -6,8 +6,8 @@ using DataFrames
 #Creación estructura generadores
 mutable struct Generadores
     IdGen::Int
-    PotMin::Float64
-    PotMax::Float64
+    PotMin:: Int
+    PotMax::Int
     GenCost::Int
     BarConexion::Int
 end
@@ -16,22 +16,22 @@ mutable struct Lineas
     IdLin::Int
     BarIni::Int
     BarFin::Int
-    PotMaxLine::Float64
+    PotMaxLine::Int
     Reactancia::Float64
 end
 
 mutable struct Barras
     IdBar::Int
-    Demanda::Vector{Float64}
+    Demanda::Vector{Int}
 end
 
 mutable struct Baterias
     IdBESS:: Int
-    Cap:: Float64
-    Horas:: Float64
+    Cap:: Int
+    Horas::Int
     Rend:: Float64
-    E_inicial_porcentaje:: Float64
-    E_final_porcentaje:: Float64
+    E_inicial::Float64
+    E_final::Float64
     BarConexion:: Int
 end    
 
@@ -85,13 +85,9 @@ set_optimizer_attribute(despacho_economico, "OutputFlag", 1) # Esto habilita la 
 @variable(despacho_economico, P_generador[g in generadores, t in Time_blocks] >= 0)
 @variable(despacho_economico, pi >= angulo_barra[b in barras, t in Time_blocks] >= -pi)
 @variable(despacho_economico, flujo[linea in lineas, t in Time_blocks]) 
-@variable(despacho_economico, energia_bat[bateria in baterias, t in Time_Aux]) 
-@variable(despacho_economico, P_from_bat[bateria in baterias, t in Time_blocks] >= 0)
-@variable(despacho_economico, P_to_bat[bateria in baterias, t in Time_blocks] >= 0) 
-
-for tiempo in Time_blocks
-    println("El tiempo es: ", tiempo)
-end    
+@variable(despacho_economico, energia_bat[bateria in baterias, t in Time_Aux] >= 0) 
+@variable(despacho_economico, potencia_bat[bateria in baterias, t in Time_blocks]) 
+ 
 
 # La función objetivo es minimizar los costos de generación
 @objective(despacho_economico, Min, sum(generador.GenCost * P_generador[generador,tiempo] for generador in generadores for tiempo in Time_blocks))
@@ -105,30 +101,19 @@ end
 # Restricción de Límite de flujo por línea
 @constraint(despacho_economico, constraint_limite_flujo[linea in lineas, tiempo in Time_blocks], - linea.PotMaxLine <= flujo[linea, tiempo] <= linea.PotMaxLine)
 # Restricción de Balance de potencia
-@constraint(despacho_economico, constraint_Power_balance[barra in barras, tiempo in Time_blocks], sum(P_generador[generador, tiempo] for generador in generadores if generador.BarConexion == barra.IdBar)
-                                                                                                + sum(sqrt(bateria.Rend)*P_from_bat[bateria, tiempo] for bateria in baterias if bateria.BarConexion == barra.IdBar)
-                                                                                                - sum(P_to_bat[bateria, tiempo] for bateria in baterias if bateria.BarConexion == barra.IdBar)
-                                                                                                - sum((flujo[linea, tiempo]) for linea in lineas if linea.BarIni == barra.IdBar)
-                                                                                                + sum((flujo[linea, tiempo]) for linea in lineas if linea.BarFin == barra.IdBar)
-                                                                                                == barra.Demanda[tiempo])
+@constraint(despacho_economico, constraint_Power_balance[barra in barras, tiempo in Time_blocks], sum(P_generador[generador, tiempo] for generador in generadores if generador.BarConexion == barra.IdBar) + sum(sqrt(bateria.Rend)*potencia_bat[bateria, tiempo] for bateria in baterias if bateria.BarConexion == barra.IdBar)- sum((flujo[linea, tiempo]) for linea in lineas if linea.BarIni == barra.IdBar) + sum((flujo[linea, tiempo]) for linea in lineas if linea.BarFin == barra.IdBar) == barra.Demanda[tiempo])
 # Restricción barra slack, para fijar en cero el ángulo de la primera barra
 @constraint(despacho_economico, constraint_barra_slack[tiempo in Time_blocks], angulo_barra[barras[1], tiempo] .== 0)
 # Restricción flujo batería.
-@constraint(despacho_economico, constraint_flujo_bateria[bateria in baterias, tiempo in Time_blocks], energia_bat[bateria, tiempo] == energia_bat[bateria, tiempo-1] + P_to_bat[bateria, tiempo]*sqrt(bateria.Rend) - P_from_bat[bateria, tiempo] )
+@constraint(despacho_economico, constraint_flujo_bateria[bateria in baterias, tiempo in Time_blocks], energia_bat[bateria, tiempo] == energia_bat[bateria, tiempo-1] - potencia_bat[bateria, tiempo]*sqrt(bateria.Rend) )
 # Restricción Condición inicial baterias
-@constraint(despacho_economico, constraint_condicion_inicial_bat[bateria in baterias], energia_bat[bateria, 0] == bateria.Cap*bateria.Horas*bateria.E_inicial_porcentaje )
+@constraint(despacho_economico, constraint_condicion_inicial_bat[bateria in baterias], energia_bat[bateria, 0] == bateria.Cap*bateria.Horas*bateria.E_inicial )
 # Restricción Condición final baterias
-@constraint(despacho_economico, constraint_condicion_final_bat[bateria in baterias], energia_bat[bateria, length(Time_blocks)] == bateria.Cap*bateria.Horas*bateria.E_final_porcentaje )
+@constraint(despacho_economico, constraint_condicion_final_bat[bateria in baterias], energia_bat[bateria, length(Time_blocks)] == bateria.Cap*bateria.Horas*bateria.E_final )
 # Restricción capacidad de energía baterias
-@constraint(despacho_economico, constraint_cap_energia_bat[bateria in baterias, tiempo in Time_blocks], 0 <= energia_bat[bateria, tiempo] <= bateria.Cap*bateria.Horas)
-# Restricción de potencia from batería
-@constraint(despacho_economico, constraint_cap_P_from_bat[bateria in baterias, tiempo in Time_blocks], P_from_bat[bateria,tiempo] <= bateria.Cap)
-# Restricción de potencia to batería
-@constraint(despacho_economico, constraint_cap_P_to_bat[bateria in baterias, tiempo in Time_blocks], P_to_bat[bateria,tiempo] <= bateria.Cap)
-# Restricción relación P from bat y P to bat
-#@constraint(despacho_economico, constraint_relacion_P_to_from_bat[bateria in baterias, tiempo in Time_blocks], P_to_bat[bateria,tiempo] * P_from_bat[bateria,tiempo] == 0)
-#Esta restricción debería estar activa para que la formulación sea completa. Sin embargo, el modelo por sí solo hace que se cumpla esta restricción al optimizar.
-#Por eso la desactivo, ya que esto permite obtener más fácil los costos marginales (en un problema MIP es muy difícil con Gurobi en Julia)
+@constraint(despacho_economico, constraint_cap_energia_bat[bateria in baterias, tiempo in Time_blocks], energia_bat[bateria, tiempo] <= bateria.Cap*3 )
+# Restricción de potencia de la batería
+@constraint(despacho_economico, constraint_cap_potencia_bat[bateria in baterias, tiempo in Time_blocks], -bateria.Cap <= potencia_bat[bateria,tiempo] <= bateria.Cap)
 
 # Resolver el modelo
 optimize!(despacho_economico)
@@ -140,48 +125,31 @@ if termination_status(despacho_economico) == MOI.OPTIMAL
         for tiempo in Time_blocks
         println("P_generador del generador ", generador.IdGen," en el tiempo ", tiempo ," es: ", value.(P_generador[generador, tiempo]))
         end
-        println("--------------------------------------------")
     end
     for linea in lineas
         for tiempo in Time_blocks
         println("Flujo de la línea ", linea.IdLin," en el tiempo ", tiempo ," es: ", value.(flujo[linea, tiempo]))
         end
-        println("--------------------------------------------")
     end
     for barra in barras
         for tiempo in Time_blocks
         println("Ángulo de la barra ", barra.IdBar," en el tiempo ", tiempo ," es: ", value.(angulo_barra[barra, tiempo]))
         end
-        println("--------------------------------------------")
     end
-    for bateria in baterias
-        for tiempo in Time_Aux 
-        println("La bateria " ,bateria.IdBESS, " en el tiempo ", tiempo, " tiene una energía de: ", value.(energia_bat[bateria,tiempo]))
-        end
-        println("--------------------------------------------")
-    end
-    for bateria in baterias
-        for tiempo in Time_blocks
-        println("La potencia desde la batería ", bateria.IdBESS," en el tiempo ", tiempo ," es: ", value.(P_from_bat[bateria, tiempo]))
-        println("La potencia hacia la batería ", bateria.IdBESS," en el tiempo ", tiempo ," es: ", value.(P_to_bat[bateria, tiempo]))
-        end
-        println("--------------------------------------------")
-    end
-    for barra in barras
-        for tiempo in Time_blocks
-            println("El costo marginal de la barra ", barra.IdBar, " en el tiempo ", tiempo, " es: ", dual(constraint_Power_balance[barra,tiempo]))
-        end
-        println("-------------------------------------------")
-    end    
     costo_total = objective_value(despacho_economico)
     println("El costo total del sistema es: ", costo_total)
+    println("--------------------------------------------")
+    for bateria in baterias
+        for tiempo in Time_Aux 
+            if tiempo == 0
+                println("La bateria " ,bateria.IdBESS, " tiene una energía de: ", value.(energia_bat[bateria,tiempo]), " en el tiempo ", tiempo)      
+            elseif (tiempo > 0 )
+                println("La bateria " ,bateria.IdBESS, " tiene una energía de: ", value.(energia_bat[bateria,tiempo]), " en el tiempo ", tiempo, "                  y una potencia de ", value.(potencia_bat[bateria, tiempo]))
+        end
+        end
+        println("--------------------------------------------")
+    end   
 
 else
     println("El modelo no pudo ser resuelto de manera óptima.")
 end
-
-
-#fixed = despacho_economico.fixed()
-##
-
-#fixed_model = get_fixed_model(despacho_economico)
