c02b415f5dd35861874ceb9f9ecae659e742ad18
Vicente Barrios <you@example.com>
2024-05-28 14:28:18 -0400

    Preg 2a lista

--- Tarea2/pregunta2a.jl
+++ Tarea2/pregunta2a.jl
@@ -49,72 +49,118 @@ interpolate_std(k1, k24, t) = k1 + (k24 - k1) * (t - 1) / 23
 lista_kt_wind = []
 lista_kt_solar = []
 for i in 1:n_horas
-    kt_i_wind = interpolate_std(14.7,30.92,i)
-    kt_i_sol = interpolate_std(10.20,14.02,i)
+    kt_i_wind = interpolate_std(0.147,0.3092,i)
+    kt_i_sol = interpolate_std(0.1020,0.1402,i)
     push!(lista_kt_wind, kt_i_wind)
     push!(lista_kt_solar, kt_i_sol)
 end
 
-escenarios = zeros(n_horas,n_escenarios) # Se crea una matriz de ceros con horas filas y n_escenarios columnas.
-
-for t in 1:n_horas
-    sigma = forecast[t]*lista_kt_t[t]
-    dist = Normal(0,sigma)
-    for escenario in 1:n_escenarios
-        escenarios[t,escenario] = max(0,forecast[t]+rand(dist))
-    end
-end
+escenarios_total_wind = zeros(n_horas,n_escenarios) # Se crea una matriz de ceros con horas filas y n_escenarios columnas.
+list_int_conf_90_dw = []
+list_int_conf_90_up = []
+list_int_conf_99_dw = []
+list_int_conf_99_up = []
 
 # Calculo escenarios total Wind #
 for t in 1:n_horas
     sigma = pronosticos[1].Potencias[t]*lista_kt_wind[t]  # Pronóstico x k_t
-    error = Normal(0,sigma)                               # Error de pronóstico
+    error = Normal(0,sigma)  # Error de pronóstico
+    push!(list_int_conf_90_dw, pronosticos[1].Potencias[t] - 1.645 * sigma) 
+    push!(list_int_conf_90_up, pronosticos[1].Potencias[t] + 1.645 * sigma)  
+    push!(list_int_conf_99_dw, pronosticos[1].Potencias[t] - 2.575 * sigma) 
+    push!(list_int_conf_99_up, pronosticos[1].Potencias[t] + 2.575 * sigma)                           
     for escenario in 1:n_escenarios
-        escenarios[t,escenario] = max(0,pronosticos[1].Potencias[t]+rand(error))
+        escenarios_total_wind[t,escenario] = max(0,pronosticos[1].Potencias[t]+rand(error))
     end
 end
-
 
 plot()
 # Iterar sobre las columnas y agregarlas al gráfico
-for i in 1:size(escenarios, 2)
-    plot!(escenarios[:, i], label="Escenario $i", linewidth=0.5)
+for i in 1:size(escenarios_total_wind, 2)
+    #plot!(escenarios_total_wind[:, i], label="Escenario $i", linewidth=0.5)
+    plot!(escenarios_total_wind[:, i], label=nothing, linewidth=0.5)
 end
+plot!(pronosticos[1].Potencias, label="Valor esperado", linewidth=4, linecolor=RGB(1.0, 1.0, 0.0), linestyle=:dash)
+plot!(list_int_conf_90_dw, label=nothing, linewidth=4,linecolor=RGB(0.0, 0.0, 1.0), linestyle=:dash)
+plot!(list_int_conf_90_up, label="Int Conf 90%", linewidth=4, linecolor=RGB(0.0, 0.0, 1.0), linestyle=:dash)
+plot!(list_int_conf_99_dw, label=nothing, linewidth=4,linecolor=RGB(1.0, 0.0, 0.0), linestyle=:dash)
+plot!(list_int_conf_99_up, label="Int Conf 99%", linewidth=4, linecolor=RGB(1.0, 0.0, 0.0), linestyle=:dash)
 # Personalizar el gráfico
 xlabel!("Horas")
 ylabel!("Potencia")
-title!("Escenarios de potencia eólica")
-
-# Ajustar el tamaño de la fuente de la leyenda
-#plot!(legendfont=font(2))
-plot!(legend=:false)
-#plot!(legend=:top, orientation=:horizontal)
-
+title!("Simulación escenarios de total eólica")
 savefig("WindEscenarios.png")
 
 
+# Calculo escenarios total Solar #
+escenarios_total_solar = zeros(n_horas,n_escenarios)
+list_int_conf_90_dw_solar = []
+list_int_conf_90_up_solar = []
+list_int_conf_99_dw_solar = []
+list_int_conf_99_up_solar = []
 for t in 1:n_horas
-    sigma = renovable.Potencias[t]*lista_kt_t[t]
-    dist = Normal(0,sigma)
+    sigma = pronosticos[2].Potencias[t]*lista_kt_solar[t]  # Pronóstico x k_t
+    error = Normal(0,sigma)                               # Error de pronóstico
+    push!(list_int_conf_90_dw_solar, pronosticos[2].Potencias[t] - 1.645 * sigma) 
+    push!(list_int_conf_90_up_solar, pronosticos[2].Potencias[t] + 1.645 * sigma)  
+    push!(list_int_conf_99_dw_solar, pronosticos[2].Potencias[t] - 2.575 * sigma) 
+    push!(list_int_conf_99_up_solar, pronosticos[2].Potencias[t] + 2.575 * sigma)  
     for escenario in 1:n_escenarios
-        #escenarios[t,escenario] = max(0,renovable.Potencias[t]+rand(dist))
-        escenarios[reno, t,escenario] = max(0,renovable.Potencias[t]+rand(dist))
+        escenarios_total_solar[t,escenario] = max(0,pronosticos[2].Potencias[t]+rand(error))
     end
 end
+plot()
+# Iterar sobre las columnas y agregarlas al gráfico
+for i in 1:size(escenarios_total_solar, 2)
+    plot!(escenarios_total_solar[:, i], label=nothing, linewidth=0.5)
+end
+plot!(pronosticos[2].Potencias, label="Valor esperado", linewidth=4, linecolor=RGB(1.0, 1.0, 0.0), linestyle=:dash)
+plot!(list_int_conf_90_dw_solar, label=nothing, linewidth=4,linecolor=RGB(0.0, 0.0, 1.0), linestyle=:dash)
+plot!(list_int_conf_90_up_solar, label="Int Conf 90%", linewidth=4, linecolor=RGB(0.0, 0.0, 1.0), linestyle=:dash)
+plot!(list_int_conf_99_dw_solar, label=nothing, linewidth=4,linecolor=RGB(1.0, 0.0, 0.0), linestyle=:dash)
+plot!(list_int_conf_99_up_solar, label="Int Conf 99%", linewidth=4, linecolor=RGB(1.0, 0.0, 0.0), linestyle=:dash)
 
-# Crear una matriz de ceros de tres dimensiones
-gen_escenarios = zeros(Float64, dim1, dim2, dim3)
-typeof(gen_escenarios)
-for renovable in pronosticos
-    if renovable.Tecnologia == "TotalWind"
-        for t in 1:n_horas
-            sigma = renovable.Potencias[t]*lista_kt_t[t]
-            dist = Normal(0,sigma)
-            for escenario in 1:n_escenarios
-                #escenarios[t,escenario] = max(0,renovable.Potencias[t]+rand(dist))
-                escenarios[t,escenario] = max(0,renovable.Potencias[t]+rand(dist))
-            end
-        end
-    end
+# Personalizar el gráfico
+xlabel!("Horas")
+ylabel!("Potencia")
+title!("Simulación escenarios de total solar")
+savefig("SolarEscenarios.png")
+
+
+
+
+# Calculo escenarios total Renovable (eólico + solar) #
+escenarios_total_renovable = zeros(n_horas,n_escenarios)
+escenarios_total_renovable = escenarios_total_wind + escenarios_total_solar 
+ 
+list_int_conf_90_dw_total = []
+list_int_conf_90_up_total = []
+list_int_conf_99_dw_total = []
+list_int_conf_99_up_total = []
+
+for t in 1:n_horas
+    sigma_wind = pronosticos[1].Potencias[t]*lista_kt_wind[t] 
+    sigma_solar = pronosticos[2].Potencias[t]*lista_kt_solar[t] 
+    sigma_total = sigma_wind+sigma_solar
+    push!(list_int_conf_90_dw_total, pronosticos[3].Potencias[t] - 1.645 * sigma_total) 
+    push!(list_int_conf_90_up_total, pronosticos[3].Potencias[t] + 1.645 * sigma_total)  
+    push!(list_int_conf_99_dw_total, pronosticos[3].Potencias[t] - 2.575 * sigma_total) 
+    push!(list_int_conf_99_up_total, pronosticos[3].Potencias[t] + 2.575 * sigma_total)  
 end
 
+plot()
+# Iterar sobre las columnas y agregarlas al gráfico
+for i in 1:size(escenarios_total_renovable, 2)
+    plot!(escenarios_total_renovable[:, i], label=nothing, linewidth=0.5)
+end
+plot!(pronosticos[3].Potencias, label="Valor esperado", linewidth=4, linecolor=RGB(1.0, 1.0, 0.0), linestyle=:dash)
+plot!(list_int_conf_90_dw_total, label=nothing, linewidth=4,linecolor=RGB(0.0, 0.0, 1.0), linestyle=:dash)
+plot!(list_int_conf_90_up_total, label="Int Conf 90%", linewidth=4, linecolor=RGB(0.0, 0.0, 1.0), linestyle=:dash)
+plot!(list_int_conf_99_dw_total, label=nothing, linewidth=4,linecolor=RGB(1.0, 0.0, 0.0), linestyle=:dash)
+plot!(list_int_conf_99_up_total, label="Int Conf 99%", linewidth=4, linecolor=RGB(1.0, 0.0, 0.0), linestyle=:dash)
+
+# Personalizar el gráfico
+xlabel!("Horas")
+ylabel!("Potencia")
+title!("Simulación escenarios de total renovable")
+savefig("RenovableEscenarios.png")
