f7300a0c9a39cb3e6abd239917fa73105e930ba5
Vicente Barrios <you@example.com>
2024-06-23 22:36:57 -0400

    arreglos graficos

--- Tarea3/pregunta2.jl
+++ Tarea3/pregunta2.jl
@@ -3,21 +3,21 @@ using Gurobi
 using SDDP
 using Plots
 using Statistics
-graph = SDDP.LinearGraph(100)   # Creamos 3 nodos y 3 arcos
+using StatsBase
+graph = SDDP.LinearGraph(100)   # Creamos 100 nodos y 100 arcos
 
 # Construcción del subproblema para cada nodo del grafo. La primera parte del subproblema es identificar las variables de estado.
 # como solo hay un estanque, solo hay una variable de estado, el volumen de la hidro [MWh].
 # El volumen va entre [0, 300], parte con 100
 function subproblem_builder(subproblem::Model, node::Int)
-    # ... stuff to go here ...
     # Variable de estado -> Volumen de estanque 
     @variable(subproblem, 0 <= volume <= 300, SDDP.State, initial_value = 100)
-    # Control variables
+    # Variables de control 
     @variables(subproblem, begin
-        gen_termico_1 >= 0
-        gen_termico_2 >= 0
-        gen_termico_3 >= 0
-        hydro_generation >= 0
+        50 >= gen_termico_1 >= 0
+        50 >= gen_termico_2 >= 0
+        50 >= gen_termico_3 >= 0
+        150 >= hydro_generation >= 0
         hydro_spill >= 0
     end)
     # Random variables
@@ -49,7 +49,7 @@ model = SDDP.PolicyGraph(
 )
 
 
-num_iteraciones = 5
+num_iteraciones = 50
 SDDP.train(model; iteration_limit = num_iteraciones)
 
 simulations = SDDP.simulate(
@@ -60,13 +60,11 @@ simulations = SDDP.simulate(
     # A list of names to record the values of.
     [:volume, :gen_termico_1, :gen_termico_2,:gen_termico_3,:hydro_generation, :hydro_spill],
 )
-
-vector_volumen =[]
-for i in 1:100
-    outgoing_volume = map(simulations[i]) do node
-        push!(vector_volumen, node[:volume].out)
-    end
-end   
+# Access a value replication 6 and stage 3
+simulations[6][2]
+# Accedo a el volumen
+simulations[6][90][:volume].out
+typeof(simulations)
 
 outgoing_volume = []
 for i in 1:100
@@ -75,8 +73,40 @@ for i in 1:100
     end
     push!(outgoing_volume, lista_aux)
 end
+mean(outgoing_volume[:][3])
 
 #outgoing_volume[1]
+
+# Media
+promedios_etapa=[]
+for etapa in 1:100
+    valores_etapa= []
+    for i in 1:100
+        push!(valores_etapa, outgoing_volume[i][etapa])
+    end
+    push!(promedios_etapa, mean(valores_etapa))
+end
+
+# Calculo de percentiles
+percentil_95 = []
+percentil_10 = []
+
+for etapa in 1:100
+    valores_etapa= []
+    for i in 1:100
+        push!(valores_etapa, outgoing_volume[i][etapa])
+    end
+    push!(percentil_95, percentile(valores_etapa, 95))
+end
+
+for etapa in 1:100
+    valores_etapa= []
+    for i in 1:100
+        push!(valores_etapa, outgoing_volume[i][etapa])
+    end
+    push!(percentil_10, percentile(valores_etapa, 10))
+end
+
 
 # Crear un gráfico vacío con etiquetas para los ejes
 plot(title="Volumen a lo largo del tiempo " * string(num_iteraciones) * " iteraciones", xlabel="Tiempo", ylabel="Volumen")
@@ -85,6 +115,9 @@ plot(title="Volumen a lo largo del tiempo " * string(num_iteraciones) * " iterac
 for i in 1:100
     plot!(1:100, outgoing_volume[i][:], label=false)  # Agregar cada columna al gráfico
 end
+plot!(1:100, promedios_etapa, label="Promedio", linecolor=:red, linewidth=2)
+plot!(1:100, percentil_95, label="Percentil 95%", linecolor=:blue, linewidth=2)
+plot!(1:100, percentil_10, label="Percentil 10%", linecolor=:black, linewidth=2)
 savefig(string(num_iteraciones) * "- iteraciones.png")
 display(plot)
 
@@ -99,11 +132,64 @@ simulations = SDDP.simulate(
     # A list of names to record the values of.
     [:volume, :gen_termico_1, :gen_termico_2,:gen_termico_3,:hydro_generation, :hydro_spill],
 )
-
+simulations[1][1]
 
 objectives = map(simulations) do simulation
     return sum(stage[:stage_objective] for stage in simulation)
 end
+
+outgoing_volume = []
+for i in 1:100
+    lista_aux = map(simulations[i]) do node
+        return node[:volume].out
+    end
+    push!(outgoing_volume, lista_aux)
+end
+
+# Media
+promedios_etapa=[]
+for etapa in 1:100
+    valores_etapa= []
+    for i in 1:100
+        push!(valores_etapa, outgoing_volume[i][etapa])
+    end
+    push!(promedios_etapa, mean(valores_etapa))
+end
+
+
+# Calculo de percentiles
+percentil_95 = []
+ic_95 = []
+
+for etapa in 1:100
+    valores_etapa= []
+    for i in 1:100
+        push!(valores_etapa, outgoing_volume[i][etapa])
+    end
+    push!(percentil_95, percentile(valores_etapa, 95))
+end
+
+for etapa in 1:100
+    valores_etapa= []
+    for i in 1:100
+        push!(valores_etapa, outgoing_volume[i][etapa])
+    end
+    push!(ic_95, mean(valores_etapa) + 1.65*std(valores_etapa))
+end
+
+# Crear un gráfico vacío con etiquetas para los ejes
+plot(title="Volumen a lo largo del tiempo " * string(num_iteraciones) * " iteraciones", xlabel="Tiempo", ylabel="Volumen")
+
+# Graficar cada columna de la matriz
+for i in 1:100
+    plot!(1:100, outgoing_volume[i][:], label=false)  # Agregar cada columna al gráfico
+end
+plot!(1:100, promedios_etapa, label="Promedio", linecolor=:red, linewidth=2)
+plot!(1:100, percentil_95, label="Percentil 95%", linecolor=:blue, linewidth=2)
+plot!(1:100, ic_95, label="IC 95%", linecolor=:black, linewidth=2)
+savefig(string(num_iteraciones) * "- iteraciones-IC.png")
+display(plot)
+
 
 lista_aux = []
 for obj in objectives
